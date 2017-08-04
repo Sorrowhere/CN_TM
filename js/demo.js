84f208376b95d8d3f407a301454f8270b991d265
sorrowhere <sorrowhere@gmail.com>
2017-08-04 15:26:00 +0800

    modify

--- js/demo.js
+++ js/demo.js
@@ -1,14 +1,34 @@
 $(function(){
 
+
+    // 历史消息弹层动画
     var $showRecord = $('.js-toggle-record');
     var $closeRecord = $('.js-close-msg');
     var $msgHistory = $('.js-msg-history');
     $showRecord.click(function(){
-        $msgHistory.removeClass('fadeOut').addClass('fadeIn');
-    }, function(){
-        $msgHistory.removeClass('fadeIn').addClass('fadeOut');
+
+        if($msgHistory.hasClass('fadeOutL')){
+            $msgHistory.removeClass('fadeOutL').addClass('fadeInL');
+        }else{
+            $msgHistory.removeClass('fadeInL').addClass('fadeOutL');
+        }
     });
+
     $closeRecord.click(function(){
-        $msgHistory.removeClass('fadeIn').addClass('fadeOut');
+        $msgHistory.removeClass('fadeInL').addClass('fadeOutL');
     });
+
+
+    // 筛选动画
+    var $searchRecord = $('.js-seach-record');
+    var $filterArea = $('.filter-area');
+    $searchRecord.click(function(){
+
+        if($filterArea.hasClass('fadeOutB')){
+            $filterArea.removeClass('fadeOutB').addClass('fadeInB');
+        }else{
+            $filterArea.removeClass('fadeInB').addClass('fadeOutB');
+        }
+    });
+
 })
